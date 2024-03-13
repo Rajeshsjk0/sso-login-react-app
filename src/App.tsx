5983895e7e662a8959f779f0a31f083aeb84fa4c
rasubramanian <v-rasubramanian@expediagroup.com>
2024-03-13 10:36:56 +0530

    add router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  function handleOnClick() {
+    alert('Button Clicked');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to React with TypeScript
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleOnClick}>SSO Login</button>
       </header>
     </div>
   );
